ea105168bfceecb07427670cc910965c1ef28e0f
Farbod Khalili Khoshehmehr <lecosmomancer@gmail.com>
2025-07-02 12:20:52 +0330

    .

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Person :
+    def __init__ (self,name,health,intrest,speaker,irritateablity):
+        self.name = name
+        self.health = health #healthy, suspect, sick
+        self.intrest = intrest #math, AI, art, economy
+        self.speaker = speaker #True, False
+        self.irritateablity = irritateablity #0, 1, 2
+map[10][10]
